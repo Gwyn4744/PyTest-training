92e6e7ba45f36aa8e0efa1358ada9f3caa7b77f5
Dominik Bednarski <dominikbednarski@wp.pl>
2023-10-18 23:37:27 +0200

    add tests for get last results with and without monkeypatch

--- tests/db_calculator_test.py
+++ tests/db_calculator_test.py
@@ -1,5 +1,6 @@
 from db_calculator import Calculator
 import pytest
+from unittest.mock import Mock
 
 
 @pytest.fixture(name='calculator', params=['no_save', 'save'])
@@ -29,3 +30,27 @@ def test_calculator_add_integers(calculator, number1, number2, result):
 def test_calculator_add_floats(calculator, number1, number2, result):
     assert calculator.add(number1, number2) == pytest.approx(result)
     calculator.remove_results_file()
+
+
+@pytest.mark.parametrize('number1, number2, result', (
+        (4, 2, 6),
+        (-3, 2, -1),
+        (0, 0, 0)
+))
+def test_get_last_result(calculator, number1, number2, result):
+    assert calculator.add(number1, number2) == result
+    assert calculator.last_result() == f'{result}'
+
+
+@pytest.mark.parametrize('number1, number2, result', (
+        (4, 2, 6),
+        (-3, 2, -1),
+        (0, 0, 0)
+))
+def test_get_last_result_mp(calculator, monkeypatch, number1, number2, result):
+    def last_result_mock():
+        return result
+    
+    monkeypatch.setattr(calculator, 'last_result', last_result_mock)
+    assert calculator.add(number1, number2) == result
+    assert calculator.last_result() == result
