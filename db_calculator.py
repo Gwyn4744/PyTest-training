5e82eead51b676e82085506498d4390737f6ec5e
Dominik Bednarski <dominikbednarski@wp.pl>
2023-10-18 13:59:12 +0200

    add remove results file feature

--- db_calculator.py
+++ db_calculator.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+
 class Calculator:
     def __init__(self, save_to_file=False):
         self.save_to_file = save_to_file
@@ -40,3 +43,16 @@ class Calculator:
     def save_result_to_file(self, result, filename="results.txt"):
         with open(filename, "a") as file:
             file.write(str(result) + "\n")
+
+    
+    def remove_results_file(self, filename="results.txt"):
+        file = Path(filename)
+        try:
+            file.unlink()
+            return f'The file {filename} has been deleted.'
+        except FileNotFoundError:
+            return f'The file {filename} does not exist.'
+        except PermissionError:
+            return f'You do not have permission to delete the {filename} file.'
+        except Exception as e:
+            return f'An error occurred while deleting file {filename}: {str(e)}'
