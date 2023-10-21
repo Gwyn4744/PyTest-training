2768ac1e96b1b8b06a4b42cc42e7a4b47e9a5938
Dominik Bednarski <dominikbednarski@wp.pl>
2023-10-21 21:28:44 +0200

    add more examples with skip decorator

--- tests/18_mark_skip_test.py
+++ tests/18_mark_skip_test.py
@@ -2,6 +2,10 @@ from db_calculator import Calculator
 import pytest
 
 
+# if True:
+#     pytest.skip("This is reason.", allow_module_level=True)
+
+
 @pytest.fixture(name='calculator')
 def fixture_init_calculator():
     calculator = Calculator()
@@ -9,17 +13,21 @@ def fixture_init_calculator():
 
 
 def test_calculator_add_integers(calculator):
-    calculator = Calculator()
     assert calculator.add(2, 3) == 5
 
 
 @pytest.mark.skip
 def test_calculator_add_integers_skip(calculator):
-    calculator = Calculator()
     assert calculator.add(2, 3) == 5
 
 
 @pytest.mark.skip(reason='any reason')
 def test_calculator_add_integers_skip_2(calculator):
-    calculator = Calculator()
+    assert calculator.add(2, 3) == 5
+
+
+
+def test_calculator_add_integers_skip_3(calculator):
+    if True:
+        pytest.skip('any other reason')
     assert calculator.add(2, 3) == 5
