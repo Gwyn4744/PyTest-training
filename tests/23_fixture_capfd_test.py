662e0e6233a41d942e2981ca668233283880d87d
Dominik Bednarski <dominikbednarski@wp.pl>
2023-10-25 21:00:55 +0200

    add test with capfd.disabled function

--- tests/23_fixture_capfd_test.py
+++ tests/23_fixture_capfd_test.py
@@ -1,4 +1,5 @@
 from db_calculator import Calculator
+import pytest
 import sys
 
 
@@ -7,9 +8,27 @@ import sys
 
 # 1
 
+# def test_system_echo(capfd):
+#     sys.stderr.write('world\n')
+#     sys.stdout.write('hello\n')
+#     captured = capfd.readouterr()
+#     assert captured.out == 'hello\n'
+#     assert captured.err == 'world\n'
+
+
+# 2
+
 def test_system_echo(capfd):
-    sys.stderr.write('world\n')
     sys.stdout.write('hello\n')
     captured = capfd.readouterr()
     assert captured.out == 'hello\n'
-    assert captured.err == 'world\n'
+    sys.stdout.write('world\n')
+    captured = capfd.readouterr()
+    assert captured.out == 'world\n'
+    with capfd.disabled():
+        sys.stdout.write('Test\n')
+    captured = capfd.readouterr()
+    with pytest.raises(AssertionError):
+        assert captured.out == 'Test\n'
+    assert captured.out == ''
+    assert captured.err == ''
